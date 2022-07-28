559e9737eb07d2bd53570ea889fc6fe7753bdba9
TheRedMaRK <pmmrkumaradasa@students.nsbm.ac.lk>
2022-07-28 10:24:44 +0530

    -CheckCarActivity.kt will not crash if there's no database entry related to the search. Exception is well handled

--- app/src/main/java/com/example/assessment_studentid/CheckCarActivity.kt
+++ app/src/main/java/com/example/assessment_studentid/CheckCarActivity.kt
@@ -1,10 +1,12 @@
 package com.example.assessment_studentid
 
 import android.annotation.SuppressLint
+import android.database.CursorIndexOutOfBoundsException
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class CheckCarActivity : AppCompatActivity() {
@@ -24,14 +26,23 @@ class CheckCarActivity : AppCompatActivity() {
 
             val cursor = db.getPrice(brand.text.toString(), model.text.toString())
 
-            cursor!!.moveToFirst()
-            price.append(cursor.getString(cursor.getColumnIndex(DBHelper.COLUMN_3)) + "/=")
-
-            while (cursor.moveToNext()) {
+            try {
+                cursor!!.moveToFirst()
                 price.append(cursor.getString(cursor.getColumnIndex(DBHelper.COLUMN_3)) + "/=")
+
+                while (cursor.moveToNext()) {
+                    price.append(cursor.getString(cursor.getColumnIndex(DBHelper.COLUMN_3)) + "/=")
+                }
+
+                cursor.close()
+
+            } catch (e: CursorIndexOutOfBoundsException) {
+                Toast.makeText(this, "Entry not found!", Toast.LENGTH_LONG).show()
+                brand.text.clear()
+                model.text.clear()
+                price.text = ""
             }
 
-            cursor.close()
         }
     }
 }
